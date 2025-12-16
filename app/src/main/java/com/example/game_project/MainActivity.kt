c9e2f70a78364def28db53013024448aca77577f
Harel <harekl429@gmail.com>
2025-12-16 15:40:18 +0200

    + Constants.kt
    Added spawning and moving cars

--- app/src/main/java/com/example/game_project/MainActivity.kt
+++ app/src/main/java/com/example/game_project/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.game_project
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.game_project.utilities.Constants
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
 
-    val car = ImageView(this)
+    private lateinit var gameLayout: LinearLayout
     private lateinit var lanes: Array<RelativeLayout>
     private lateinit var player: ImageView
     private var playerPos=1; ///  0 - left lane 1- middle lane 2- right lane
@@ -30,16 +37,14 @@ class MainActivity : AppCompatActivity() {
         val btnLeft: ImageButton = findViewById(R.id.left_button)
         val btnRight: ImageButton = findViewById(R.id.right_button)
         player = findViewById(R.id.player)
-
+        gameLayout = findViewById(R.id.gameLayout)
         lanes = arrayOf(findViewById(R.id.left_lane)  ,  findViewById(R.id.center_lane)  ,  findViewById(R.id.right_lane))
         btnLeft.setOnClickListener { moveLeft() }
         btnRight.setOnClickListener { moveRight() }
 
-        car.setImageResource(R.drawable.car)
-        car.layoutParams = FrameLayout.LayoutParams(120, 120)
-
         player.post { updatePlayerPosition() }
-
+        startSpawning()
+        gameLoop()
     }
     private fun moveLeft()
     {
@@ -49,7 +54,6 @@ class MainActivity : AppCompatActivity() {
             updatePlayerPosition()
         }
     }
-
     private fun moveRight()
     {
         if(playerPos < 2)
@@ -63,5 +67,59 @@ class MainActivity : AppCompatActivity() {
         val parent = player.parent as ViewGroup
         parent.removeView(player)
         lanes[playerPos].addView(player);
+    }
+
+    private val obstacles = mutableListOf<ImageView>()
+    private val spawnHandler = Handler(Looper.getMainLooper())
+
+    private fun spawnCar() {
+        val car = ImageView(this)
+        car.setImageResource(R.drawable.car)
+        car.layoutParams = FrameLayout.LayoutParams(
+            player.width,
+            player.height
+        )
+
+        // Choose random lane
+        val laneIndex = (0..2).random()
+        car.y = -player.height.toFloat()
+
+        lanes[laneIndex].addView(car)
+        obstacles.add(car)
+    }
+    private fun startSpawning() {
+        spawnHandler.postDelayed(object : Runnable {
+            override fun run() {
+                spawnCar()
+                spawnHandler.postDelayed(this, Constants.Car.SPAWN_DELAY) // spawn every 1.2s
+            }
+        }, Constants.Car.SPAWN_DELAY)
+    }
+
+    private val gameHandler = Handler(Looper.getMainLooper())
+    private val speed = 12f
+
+    private fun gameLoop() {
+        gameHandler.post(object : Runnable {
+            override fun run() {
+
+                val iterator = obstacles.iterator()
+                while (iterator.hasNext()) {
+                    val car = iterator.next()
+                    car.y += speed
+
+                    if (car.y > gameLayout.height) {
+                        gameLayout.removeView(car)
+                        iterator.remove()
+                        continue
+                    }
+
+                    ///// need to make collission check
+
+                }
+
+                gameHandler.postDelayed(this, Constants.Car.MOVE_DELAY)
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/game_project/utilities/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.game_project.utilities
+
+class Constants {
+
+    object Car
+    {
+        const val SPEED: Float =12f
+        const val SPAWN_DELAY: Long = 1_200L
+        const val MOVE_DELAY: Long = 16L
+    }
+}
