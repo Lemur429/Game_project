dcba4abc43d107afc24ab117c2e23e7a725e870b
Harel <harekl429@gmail.com>
2025-12-16 16:22:53 +0200

    player gets damaged game gets restarted

--- app/src/main/java/com/example/game_project/MainActivity.kt
+++ app/src/main/java/com/example/game_project/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageButton
@@ -14,16 +15,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.contains
 import com.example.game_project.utilities.Constants
-import java.util.Timer
-import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var gameLayout: LinearLayout
     private lateinit var lanes: Array<RelativeLayout>
+
+    /// ---------- PLAYER VARIABLES
     private lateinit var player: ImageView
     private var playerPos=1; ///  0 - left lane 1- middle lane 2- right lane
+    private var hearts=3;
+    private lateinit var hearts_view: Array<ImageView>
+    /// ---------- /PLAYER VARIABLES/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         player = findViewById(R.id.player)
         gameLayout = findViewById(R.id.gameLayout)
         lanes = arrayOf(findViewById(R.id.left_lane)  ,  findViewById(R.id.center_lane)  ,  findViewById(R.id.right_lane))
+        hearts_view = arrayOf( findViewById(R.id.heart1) , findViewById(R.id.heart2) , findViewById(R.id.heart3))
         btnLeft.setOnClickListener { moveLeft() }
         btnRight.setOnClickListener { moveRight() }
 
@@ -68,7 +74,11 @@ class MainActivity : AppCompatActivity() {
         parent.removeView(player)
         lanes[playerPos].addView(player);
     }
-
+    private fun updateHearts() {
+        hearts_view[0].visibility = if (hearts >= 1) View.VISIBLE else View.GONE
+        hearts_view[1].visibility = if (hearts >= 2) View.VISIBLE else View.GONE
+        hearts_view[2].visibility = if (hearts >= 3) View.VISIBLE else View.GONE
+    }
     private val obstacles = mutableListOf<ImageView>()
     private val spawnHandler = Handler(Looper.getMainLooper())
 
@@ -87,6 +97,10 @@ class MainActivity : AppCompatActivity() {
         lanes[laneIndex].addView(car)
         obstacles.add(car)
     }
+    private fun stopSpawning()
+    {
+        spawnHandler.removeCallbacksAndMessages(null)
+    }
     private fun startSpawning() {
         spawnHandler.postDelayed(object : Runnable {
             override fun run() {
@@ -99,6 +113,24 @@ class MainActivity : AppCompatActivity() {
     private val gameHandler = Handler(Looper.getMainLooper())
     private val speed = 12f
 
+    private fun gameOver()
+    {
+        stopSpawning()
+        val iterator = obstacles.iterator()
+        while (iterator.hasNext())
+        {
+            val car = iterator.next()
+            (car.parent as RelativeLayout).removeView(car);
+            iterator.remove()
+        }
+        hearts =3;
+        updateHearts()
+        playerPos=1;
+        updatePlayerPosition()
+        startSpawning()
+        gameLoop()
+
+    }
     private fun gameLoop() {
         gameHandler.post(object : Runnable {
             override fun run() {
@@ -109,17 +141,30 @@ class MainActivity : AppCompatActivity() {
                     car.y += speed
 
                     if (car.y > gameLayout.height) {
-                        gameLayout.removeView(car)
+                        (car.parent as RelativeLayout).removeView(car);
                         iterator.remove()
                         continue
                     }
 
+
                     ///// need to make collission check
+                    if (car.y + car.layoutParams.height > player.y &&  car.y < player.y+ car.layoutParams.height/2 && lanes[playerPos].contains(car))
+                    {
+                        hearts--;
+                        (car.parent as RelativeLayout).removeView(car);
+                        iterator.remove()
+                        updateHearts()
 
+                        continue
+                    }
                 }
-
-                gameHandler.postDelayed(this, Constants.Car.MOVE_DELAY)
+                if (hearts > 0)
+                    gameHandler.postDelayed(this, Constants.Car.MOVE_DELAY)
+                else
+                    gameOver()
             }
+
+
         })
     }
 }
